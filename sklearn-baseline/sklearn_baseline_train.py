9999f77363db43af8ad5387258f755fcab30b00b
Sharonin Vladislav <vladsharonin@gmail.com>
2023-11-29 11:22:41 +0100

    Some preprocessing magic

--- sklearn-baseline/sklearn_baseline_train.py
+++ sklearn-baseline/sklearn_baseline_train.py
@@ -2,11 +2,14 @@
 import argparse
 import jsonlines
 import pandas as pd
+from nltk import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 import os
 import joblib
+from bs4 import BeautifulSoup
+from nltk.stem import PorterStemmer
 
 def load_data(file_path):
     with jsonlines.open(file_path) as reader:
@@ -28,12 +31,20 @@ def parse_args():
 
 def preprocess(content):
     # Placeholder for the content preprocessing
-    return content
+    #return content
+    #----------
+    #soup = BeautifulSoup(content, 'html.parser')
+    #return soup.get_text()
+    #---------
+    stemmer = PorterStemmer()
+    words = word_tokenize(content)
+    stemmed_content = [stemmer.stem(word) for word in words]
+    return ' '.join(stemmed_content)
 
 def main(data_dir, model_output):
     # Load datasets
-    train_data = load_data(os.path.join(data_dir, 'train/D1_train.jsonl'))
-    train_labels = load_data(os.path.join(data_dir, 'train/D1_train-truth.jsonl'))['label']
+    train_data = load_data(os.path.join(data_dir, 'C:/Users/vlads/Downloads/webpage-classification-irixys23-team-01-main/webpage-classification-irixys23-team-01-main/sklearn-baseline/train/D1_train.jsonl'))
+    train_labels = load_data(os.path.join(data_dir, 'C:/Users/vlads/Downloads/webpage-classification-irixys23-team-01-main/webpage-classification-irixys23-team-01-main/sklearn-baseline/train/D1_train-truth.jsonl'))['label']
 
     # Preprocess the content
     train_data['url'] = train_data['url'].apply(preprocess)
@@ -47,9 +58,17 @@ def main(data_dir, model_output):
     # Train the model
     pipeline.fit(train_data['url'], train_labels)
 
+    # Create the directory if it doesn't exist
+    os.makedirs(os.path.dirname(model_output), exist_ok=True)
+
     # Save the trained model
     joblib.dump(pipeline, model_output)
 
 if __name__ == "__main__":
     args = parse_args()
     main(args.data_dir, args.model_output)
+
+
+
+    #train_data = load_data(os.path.join(data_dir, 'C:/Users/vlads/Downloads/webpage-classification-irixys23-team-01-main/webpage-classification-irixys23-team-01-main/sklearn-baseline/train/D1_train.jsonl'))
+    #train_labels = load_data(os.path.join(data_dir, 'C:/Users/vlads/Downloads/webpage-classification-irixys23-team-01-main/webpage-classification-irixys23-team-01-main/sklearn-baseline/train/D1_train-truth.jsonl'))['label']
